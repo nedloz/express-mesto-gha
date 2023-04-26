c24da62067624cc158b78fd3ab7b42af52bce96b
nedloz <andrukuzminkuzmin@yandex.ru>
2023-04-27 00:07:01 +0300

    ПР 14 7

--- app.js
+++ app.js
@@ -55,6 +55,10 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
-    .send({ message });
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 });
 app.listen(PORT);
